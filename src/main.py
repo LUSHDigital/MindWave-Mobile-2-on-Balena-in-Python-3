264e4909bcf591df6d9b98e6dddd8d52ada6c17a
Simon-Ince <simon.ince@lush.co.uk>
2019-08-12 14:48:35 +0100

    created dynamic flask template

--- src/main.py
+++ src/main.py
@@ -1,9 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/suggestions")
+def suggestions():
+    text = request.args.get("jsdata")
+
+    suggestions_list = []
+
+    if text:
+        r = requests.get(
+            "http://suggestqueries.google.com/complete/search?output=toolbar&hl=ru&q={}&gl=in".format(
+                text
+            )
+        )
+
+        soup = BeautifulSoup(r.content, "lxml")
+
+        suggestions = soup.find_all("suggestion")
+
+        for suggestion in suggestions:
+            suggestions_list.append(suggestion.attrs["data"])
+
+        # print(suggestions_list)
+
+    return render_template("suggestions.html", suggestions=suggestions_list)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
