1274ada0c53d200618b531d7b5320deb5f591300
Simon-Ince <simon.ince@lush.co.uk>
2019-08-15 12:50:39 +0100

    shot in the dark

--- src/main.py
+++ src/main.py
@@ -20,9 +20,7 @@ def index():
                 yield "%s<br/>\n" % x
 
         result = inner
-    env = Environment(loader=FileSystemLoader(template_dir))
-    tmpl = env.get_template("result.html")
-    return flask.Response(tmpl.generate(result=result if result is None else result()))
+    return render_template("result.html", result=result if result is None else result())
 
 
 if __name__ == "__main__":
