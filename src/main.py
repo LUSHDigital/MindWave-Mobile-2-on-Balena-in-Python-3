978604b3bec90336a7d039e28eead12f82aabf3b
Simon-Ince <simon.ince@lush.co.uk>
2019-08-15 09:26:48 +0100

    added endpoint for results

--- src/main.py
+++ src/main.py
@@ -46,5 +46,30 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/suggestions")
+def suggestions():
+    text = request.args.get("jsdata")
+
+    suggestions_list = []
+
+    if text:
+        r = requests.get(
+            "http://suggestqueries.google.com/complete/search?output=toolbar&hl=ru&q={}&gl=in".format(
+                text
+            )
+        )
+
+        soup = BeautifulSoup(r.content, "lxml")
+
+        suggestions = soup.find_all("suggestion")
+
+        for suggestion in suggestions:
+            suggestions_list.append(suggestion.attrs["data"])
+
+        # print(suggestions_list)
+
+    return render_template("suggestions.html", suggestions=suggestions_list)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
