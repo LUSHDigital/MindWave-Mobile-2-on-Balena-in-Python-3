31f0ebb56d629bc8d62da3ddbac6b0a5e30463e6
Simon-Ince <simon.ince@lush.co.uk>
2019-08-15 11:54:13 +0100

    shot in the dark

--- src/main.py
+++ src/main.py
@@ -43,5 +43,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/get_word")
+def get_prediction():
+    word = flask.request.args.get("word")
+    return flask.jsonify({"html": getPrediction(word)})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
