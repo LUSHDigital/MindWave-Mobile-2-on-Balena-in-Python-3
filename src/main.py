346328e6866680b6b68300f041c1822f95dc5107
Simon-Ince <simon.ince@lush.co.uk>
2019-08-15 14:59:47 +0100

    shot in the dark

--- src/main.py
+++ src/main.py
@@ -1,28 +1,21 @@
 import flask
-import time
-
-from flask import request
-from jinja2 import Environment
-from jinja2.loaders import FileSystemLoader
+from shelljob import proc
 
 app = flask.Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    result = None
-    if request.method == "POST":
-        counter = int(request.form.get("counter", 0))
+@app.route("/stream")
+def stream():
+    g = proc.Group()
+    p = g.run(["bash", "-c", "for ((i=0;i<100;i=i+1)); do echo $i; sleep 1; done"])
 
-        def inner():
-            for x in range(counter):  # eg: 100 should input value from browser
-                time.sleep(1)
-                yield "%s<br/>\n" % x
+    def read_process():
+        while g.is_pending():
+            lines = g.readlines()
+            for proc, line in lines:
+                yield line
 
-        result = inner
-    env = Environment(loader=FileSystemLoader("templates"))
-    tmpl = env.get_template("result.html")
-    return flask.Response(tmpl.generate(result=result if result is None else result()))
+    return flask.Response(read_process(), mimetype="text/plain")
 
 
 if __name__ == "__main__":
