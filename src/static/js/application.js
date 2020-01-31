753b89cd2f48df49ea769954cf334c793dbdbab1
Simon-Ince <simon.w.ince@gmail.com>
2020-01-31 16:48:21 +0000

    set graph data from dict

--- src/static/js/application.js
+++ src/static/js/application.js
@@ -17,60 +17,60 @@ $(document).ready(function(){
     // console.log(msg.output);
     chartData = JSON.parse(msg.output);
     console.log(chartData);
-    //
-    // new Chart(document.getElementById("line-chart"), {
-    //   type: 'line',
-    //   data: {
-    //     labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-    //     datasets: [{
-    //         data: chartData.delta,
-    //         label: "Delta",
-    //         borderColor: "#3e95cd",
-    //         fill: false
-    //       }, {
-    //         data: chartData.theta,
-    //         label: "Theta",
-    //         borderColor: "#8e5ea2",
-    //         fill: false
-    //       }, {
-    //         data: chartData.lowAlpha,
-    //         label: "lowAlpha",
-    //         borderColor: "#3cba9f",
-    //         fill: false
-    //       }, {
-    //         data: chartData.highAlpha,
-    //         label: "highAlpha",
-    //         borderColor: "#e8c3b9",
-    //         fill: false
-    //       }, {
-    //         data: chartData.lowBeta,
-    //         label: "lowBeta",
-    //         borderColor: "#c45850",
-    //         fill: false
-    //       }, {
-    //         data: chartData.highBeta,
-    //         label: "highBeta",
-    //         borderColor: "#c45850",
-    //         fill: false
-    //       }, {
-    //         data: chartData.lowGamma,
-    //         label: "lowGamma",
-    //         borderColor: "#c45850",
-    //         fill: false
-    //       }, {
-    //         data: chartData.midGamma,
-    //         label: "midGamma",
-    //         borderColor: "#c45850",
-    //         fill: false
-    //       }
-    //     ]
-    //   },
-    //   options: {
-    //     title: {
-    //       display: true,
-    //       text: 'World population per region (in millions)'
-    //     }
-    //   }
-    // });
+
+    new Chart(document.getElementById("line-chart"), {
+      type: 'line',
+      data: {
+        labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+        datasets: [{
+            data: chartData.delta,
+            label: "Delta",
+            borderColor: "#3e95cd",
+            fill: false
+          }, {
+            data: chartData.theta,
+            label: "Theta",
+            borderColor: "#8e5ea2",
+            fill: false
+          }, {
+            data: chartData.lowAlpha,
+            label: "lowAlpha",
+            borderColor: "#3cba9f",
+            fill: false
+          }, {
+            data: chartData.highAlpha,
+            label: "highAlpha",
+            borderColor: "#e8c3b9",
+            fill: false
+          }, {
+            data: chartData.lowBeta,
+            label: "lowBeta",
+            borderColor: "#c45850",
+            fill: false
+          }, {
+            data: chartData.highBeta,
+            label: "highBeta",
+            borderColor: "#c45850",
+            fill: false
+          }, {
+            data: chartData.lowGamma,
+            label: "lowGamma",
+            borderColor: "#c45850",
+            fill: false
+          }, {
+            data: chartData.midGamma,
+            label: "midGamma",
+            borderColor: "#c45850",
+            fill: false
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'World population per region (in millions)'
+        }
+      }
+    });
   });
 });
