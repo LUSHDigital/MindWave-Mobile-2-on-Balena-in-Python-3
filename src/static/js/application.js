97e8085e9d7977b7b758728b95a49d378f8419d5
Simon-Ince <simon.w.ince@gmail.com>
2020-02-03 11:47:22 +0000

    changed chart ro update

--- src/static/js/application.js
+++ src/static/js/application.js
@@ -6,7 +6,57 @@ $(document).ready(function(){
   var chart = new Chart(document.getElementById("line-chart"), {
     type: 'line',
     data: {
-      labels: [9,8,7,6,5,4,3,2,1,0]
+      labels: [9,8,7,6,5,4,3,2,1,0],
+      datasets: [
+        {
+          data: [],
+          label: "delta",
+          borderColor: "#3e95cd",
+          fill: false
+        },
+        {
+          data: [],
+          label: "theta",
+          borderColor: "#8e5ea2",
+          fill: false
+        },
+        {
+          data: [],
+          label: "lowAlpha",
+          borderColor: "#3cba9f",
+          fill: false
+        },
+        {
+          data: [],
+          label: "highAlpha",
+          borderColor: "#e8c3b9",
+          fill: false
+        },
+        {
+          data: [],
+          label: "lowBeta",
+          borderColor: "#c45850",
+          fill: false
+        },
+        {
+          data: [],
+          label: "highBeta",
+          borderColor: "#c45850",
+          fill: false
+        },
+        {
+          data: [],
+          label: "lowGamma",
+          borderColor: "#c45850",
+          fill: false
+        },
+        {
+          data: [],
+          label: "midGamma",
+          borderColor: "#c45850",
+          fill: false
+        }
+      ]
     },
     options: {
       animation: {
@@ -15,61 +65,23 @@ $(document).ready(function(){
     }
   });
 
-    //receive details from server
-    socket.on('newnumber', function(msg) {
-      chartData = JSON.parse(msg.output);
-      console.log(chartData);
+  //receive details from server
+  socket.on('newnumber', function(msg) {
+    chartData = JSON.parse(msg.output);
+    console.log(chartData);
 
+    chart.data.datasets.forEach(dataset => {
+      var name = dataset.label;
+      var value = chartData[name][0];
+      dataset.data.push(value);
+    });
 
-      // function addData(chart, label, data) {
-        // chart.data.labels.push(label);
-        chart.data.datasets = [{
-            data: Object.values(chartData.delta),
-            label: "Delta",
-            borderColor: "#3e95cd",
-            fill: false
-          }, {
-            data: Object.values(chartData.theta),
-            label: "Theta",
-            borderColor: "#8e5ea2",
-            fill: false
-          }, {
-            data: Object.values(chartData.lowAlpha),
-            label: "lowAlpha",
-            borderColor: "#3cba9f",
-            fill: false
-          }, {
-            data: Object.values(chartData.highAlpha),
-            label: "highAlpha",
-            borderColor: "#e8c3b9",
-            fill: false
-          }, {
-            data: Object.values(chartData.lowBeta),
-            label: "lowBeta",
-            borderColor: "#c45850",
-            fill: false
-          }, {
-            data: Object.values(chartData.highBeta),
-            label: "highBeta",
-            borderColor: "#c45850",
-            fill: false
-          }, {
-            data: Object.values(chartData.lowGamma),
-            label: "lowGamma",
-            borderColor: "#c45850",
-            fill: false
-          }, {
-            data: Object.values(chartData.midGamma),
-            label: "midGamma",
-            borderColor: "#c45850",
-            fill: false
-          }
-        ];
-        chart.options = {
-          animation: {
-            duration: 0
-          }
-        };
-        chart.update();
+    chart.data.datasets.forEach(dataset => {
+      if (dataset.data.length === 11) {
+        dataset.data.shift();
+      }
+    });
+
+    chart.update();
   });
 });
