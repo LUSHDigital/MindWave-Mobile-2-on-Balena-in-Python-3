06c6b0a4e973aa2cad7210cd053628e31eff7ed7
Simon-Ince <simon.w.ince@gmail.com>
2020-02-03 13:50:42 +0000

    trying data streaming

--- src/static/js/application.js
+++ src/static/js/application.js
@@ -1,89 +1,154 @@
 $(document).ready(function(){
-  //connect to the socket server.
-  var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
-  var numbers_received = [];
 
-  var chart = new Chart(document.getElementById("line-chart"), {
+var samples = 20;
+var speed = 250;
+let timeout = samples * speed;
+var values = {
+  labels: [9,8,7,6,5,4,3,2,1,0],
+  datasets: [
+    {
+      data: [],
+      label: "delta",
+      borderColor: "#3e95cd",
+      fill: false
+    },
+    {
+      data: [],
+      label: "theta",
+      borderColor: "#8e5ea2",
+      fill: false
+    },
+    {
+      data: [],
+      label: "lowAlpha",
+      borderColor: "#3cba9f",
+      fill: false
+    },
+    {
+      data: [],
+      label: "highAlpha",
+      borderColor: "#e8c3b9",
+      fill: false
+    },
+    {
+      data: [],
+      label: "lowBeta",
+      borderColor: "#c45850",
+      fill: false
+    },
+    {
+      data: [],
+      label: "highBeta",
+      borderColor: "#c45850",
+      fill: false
+    },
+    {
+      data: [],
+      label: "lowGamma",
+      borderColor: "#c45850",
+      fill: false
+    },
+    {
+      data: [],
+      label: "midGamma",
+      borderColor: "#c45850",
+      fill: false
+    }
+  ]
+};
+var labels = [];
+var charts = [];
+var value = 0;
+var scale = 1;
+
+addEmptyValues(values, samples);
+
+
+
+var originalCalculateXLabelRotation = Chart.Scale.prototype.calculateXLabelRotation
+
+function initialize() {
+  charts.push(new Chart(document.getElementById("chart0"), {
     type: 'line',
     data: {
-      labels: [9,8,7,6,5,4,3,2,1,0],
-      datasets: [
-        {
-          data: [],
-          label: "delta",
-          borderColor: "#3e95cd",
-          fill: false
-        },
-        {
-          data: [],
-          label: "theta",
-          borderColor: "#8e5ea2",
-          fill: false
-        },
-        {
-          data: [],
-          label: "lowAlpha",
-          borderColor: "#3cba9f",
-          fill: false
-        },
-        {
-          data: [],
-          label: "highAlpha",
-          borderColor: "#e8c3b9",
-          fill: false
-        },
-        {
-          data: [],
-          label: "lowBeta",
-          borderColor: "#c45850",
-          fill: false
-        },
-        {
-          data: [],
-          label: "highBeta",
-          borderColor: "#c45850",
-          fill: false
-        },
-        {
-          data: [],
-          label: "lowGamma",
-          borderColor: "#c45850",
-          fill: false
-        },
-        {
-          data: [],
-          label: "midGamma",
-          borderColor: "#c45850",
-          fill: false
-        }
-      ]
+      //labels: labels,
+      datasets: [{
+        data: values,
+        backgroundColor: 'rgba(255, 99, 132, 0.1)',
+        borderColor: 'rgb(255, 99, 132)',
+        borderWidth: 2,
+        lineTension: 0.25,
+        pointRadius: 0
+      }]
     },
     options: {
-        animation: {
-            onProgress: function(animation) {
-                progress.value = animation.animationObject.currentStep / animation.animationObject.numSteps;
-            }
-        }
-    }
-  });
-
-  //receive details from server
-  socket.on('newnumber', function(msg) {
-    chartData = JSON.parse(msg.output);
-    console.log(chartData);
-
-    chart.data.datasets.forEach(dataset => {
-      var name = dataset.label;
-      var value = chartData[name][0];
-      dataset.data.push(value);
-    });
-
-    chart.data.datasets.forEach(dataset => {
-      if (dataset.data.length === 11) {
-        dataset.data.shift();
+      responsive: true,
+      animation: {
+        duration: speed * 1.5,
+        easing: 'linear'
+      },
+      legend: false,
+      scales: {
+        xAxes: [{
+          type: "time",
+          display: true
+        }],
+        yAxes: [{
+          ticks: {
+            max: 1,
+            min: -1
+          }
+        }]
       }
-    });
+    }
+  }));
+}
 
+function addEmptyValues(arr, n) {
+  for(var i = 0; i < n; i++) {
+    arr.push({
+      x: moment().subtract((n - i) * speed, 'milliseconds').toDate(),
+      y: null
+    });
+  }
+}
+
+function rescale() {
+  var padding = [];
+
+  addEmptyValues(padding, 10);
+  values.splice.apply(values, padding);
+
+  scale++;
+}
+
+function updateCharts(){
+  charts.forEach(function(chart) {
     chart.update();
   });
+}
+
+
+window.onload = function() {
+  initialize();
+};
+
+//receive details from server
+socket.on('newnumber', function(msg) {
+  chartData = JSON.parse(msg.output);
+  console.log(chartData);
+
+  values.datasets.forEach(dataset => {
+    var name = dataset.label;
+    var value = chartData[name][0];
+    dataset.data.push(value);
+  });
+
+  values.datasets.forEach(dataset => {
+    if (dataset.data.length === 11) {
+      dataset.data.shift();
+    }
+  });
+  updateCharts();
+});
 });
